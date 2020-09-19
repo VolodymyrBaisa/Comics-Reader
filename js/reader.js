8435923b3533ee290d53e14375f01f73f7fd5614
Paige Burlew <paigeburlew@gmail.com>
2020-09-19 09:13:29 -0500

    ajax set up

--- js/reader.js
+++ js/reader.js
@@ -1,9 +1,19 @@
 //API
 const api1Query = "https://project1api1.herokuapp.com";
 const api2Query = "https://project1api2.herokuapp.com";
+//api key:
+//sample query:
+
 //Variable
 
 // read url 
+
+fullQueryURL = "" + "" + "";
+    $.ajax({
+        url:
+        method: "GET"
+    }).then
+
 // get comic id from url
 // call comic API to get the relevant comic
 // create HTML elements to show it on the page (0)
