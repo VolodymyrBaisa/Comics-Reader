7153ba3864828354f07ee7adc217696896c33dbc
Paige Burlew <paigeburlew@gmail.com>
2020-09-18 21:12:10 -0500

    js update

--- js/reader.js
+++ js/reader.js
@@ -2,8 +2,8 @@
 const api1Query = "https://project1api1.herokuapp.com";
 const api2Query = "https://project1api2.herokuapp.com";
 //Variable
-//Selector
 
-//Events
-//Class
-//Function
+// read url 
+// get comic id from url
+// call comic API to get the relevant comic
+// create HTML elements to show it on the page (0)
