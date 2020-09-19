4f24dee624c591aa96f4547924d09f7bd2e81b51
Volodymyr Baisa <biosvova@gmail.com>
2020-09-19 17:13:22 -0400

    improving icons

--- js/reader.js
+++ js/reader.js
@@ -1,9 +1,10 @@
 //API
 const api1Query = "https://project1api1.herokuapp.com";
-const api2Query = "https://project1api2.herokuapp.com";
 //Variable
 //Selector
 
 //Events
 //Class
 //Function
+//Init
+(() => {})();
